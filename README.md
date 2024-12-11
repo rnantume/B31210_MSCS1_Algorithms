f0070f78f71119c7fd4ebc66d5003317a09dc0ba
unknown <nnrobin37@gmail.com>
2024-12-11 16:16:32 +0300

    Program that utilises the structure added

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 
 #Hybrid data structurs
 ##It combines the strengths of binary search trees(BSTs) and hash tables to optimize operations like search, insertion, and deletion. Traditional BSTs offer ordered storage and efficient traversal but may degrade to linear time complexity in the worst case. Hash tables provide constant-time average-case operations but lack ordered data access. 
+
+
+Tests identified to be done:
+- dataset1 with integer keys and dataset2 with string keys
+- test duplicate keys: if it replaces entries of same key (check if item (15,"len) exists in the tree after insertion dataset1)
+- test insertion, deletion, searching, and structure after deletion and insertion
+-  test with varying data sizes
+- test with integer keys and string keys
+- test with sorted dataset
+- test collision: keys resolve to the same bucket index 
+- test range queries
+- test with varrying table size
---
+++ hybrid_hash_with_bst.py
@@ -0,0 +1,48 @@
+from HybridDS import BST_Node, Binary_Search_Tree, Hybrid_DS
+
+#Program that utilizes the Hybrid Hash-BST data structure
+def main():
+    # Initialize the Hybrid_DS with a table size of 6
+    hybrid = Hybrid_DS(6)
+
+    dataset1= [(10, "bae"), (5, "tim"), (15, "len"), 
+              (20, "moe"), (18, "mia"), (25, "zoe"), 
+              (15, "sue"), (12, "lou"), (18, "rae")] 
+
+    dataset2 = [("bae", "Kenya"), ("tim", "Uganda"), ("len", "Tanzania"),
+                 ("moe", "Rwanda"), ("mia", "DRC"), ("zoe", "S.Sudan"), 
+                 ("sue", "Egypt"), ("lou", "Uganda"), ("rae", "Kenya")]
+
+    print("Inserting data from dataset1:")
+    for key, value in dataset1:
+        hybrid.insert(key, value)
+        print(f"Inserted ({key}, {value})")
+
+    print("\nSearching for keys in dataset1:")
+    search_keys = [15, 20, 50]  # keys may exist or not
+    for key in search_keys:
+        result = hybrid.search(key)
+        if result:
+            print(f"Key {key} found with value: {result}")
+        else:
+            print(f"Key {key} not found.")
+
+    print("\nDeleting keys from dataset1:")
+    delete_keys = [20, 35, 50]  # keys may exist or not
+    for key in delete_keys:
+        success = hybrid.delete(key)
+        if success:
+            print(f"Key {key} deleted successfully.")
+        else:
+            print(f"Key {key} not found for deletion.")
+
+    print("\nSearching again after deletions:")
+    for key in search_keys:
+        result = hybrid.search(key)
+        if result:
+            print(f"Key {key} found with value: {result}")
+        else:
+            print(f"Key {key} not found.")
+
+if __name__ == "__main__":
+    main()
